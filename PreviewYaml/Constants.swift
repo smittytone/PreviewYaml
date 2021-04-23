4981b4138b127ee3e53afb6c50b9d13205c28412
Tony Smith <smittytone@icloud.com>
2021-04-23 18:02:03 +0100

    Change constants for this app

--- PreviewYaml/Constants.swift
+++ PreviewYaml/Constants.swift
@@ -15,7 +15,7 @@ struct BUFFOON_CONSTANTS {
     struct ERRORS {
 
         struct CODES {
-            static let NONE                 = 0
+            static let NONE                = 0
             static let FILE_INACCESSIBLE    = 400
             static let FILE_WONT_OPEN       = 401
             static let BAD_MD_STRING        = 402
@@ -26,7 +26,7 @@ struct BUFFOON_CONSTANTS {
             static let NO_ERROR            = "No error"
             static let FILE_INACCESSIBLE    = "Can't access file"
             static let FILE_WONT_OPEN       = "Can't open file"
-            static let BAD_MD_STRING        = "Can't get markdown data"
+            static let BAD_MD_STRING        = "Can't get yaml data"
             static let BAD_TS_STRING        = "Can't access NSTextView's TextStorage"
         }
     }
@@ -38,22 +38,16 @@ struct BUFFOON_CONSTANTS {
         static let WIDTH                = 768
         static let HEIGHT               = 1024
         static let ASPECT               = 0.75
-        static let TAG_HEIGHT         = 180
+        static let TAG_HEIGHT          = 180
     }
 
     static let BASE_PREVIEW_FONT_SIZE       = 16.0
-    static let BASE_THUMB_FONT_SIZE         = 14.0
-    static let SPACES_FOR_A_TAB             = 4
+    static let BASE_THUMB_FONT_SIZE         = 16.0
 
-    // FROM 1.2.0
     static let CODE_COLOUR_INDEX            = 0
-    static let LINK_COLOUR_INDEX            = 2
-
     static let CODE_FONT_INDEX             = 0
-    static let BODY_FONT_INDEX             = 0
 
     static let FONT_SIZE_OPTIONS: [CGFloat] = [10.0, 12.0, 14.0, 16.0, 18.0, 24.0, 28.0]
     
-    // FROM 1.3.0
     static let YAML_INDENT                  = 2
 }
