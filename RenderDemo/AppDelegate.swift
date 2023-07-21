5b617a6af13638b8e02bfc9ccc23bf8b7b346839
smittytone <smitty@smittytone.net>
2023-07-21 12:10:18 +0100

    Add light mode to render demo

--- RenderDemo/AppDelegate.swift
+++ RenderDemo/AppDelegate.swift
@@ -12,89 +12,137 @@ class AppDelegate:  NSObject,
                     NSApplicationDelegate,
                     NSOpenSavePanelDelegate {
 
-
+    // MARK: - Class UI Properies
+    
     @IBOutlet var window: NSWindow!
     @IBOutlet var mainView: NSView!
-    @IBOutlet var filePathLabel: NSTextField!
     @IBOutlet var previewTextView: NSTextView!
     @IBOutlet var previewScrollView: NSScrollView!
+    @IBOutlet var modeButton: NSButton!
 
+
+    // MARK: - Private Properies
 
     private var openDialog: NSOpenPanel? = nil
-    private var isOpenDialogVisible: Bool = false
+    private var currentURL: URL? = nil
+    private var renderAsDark: Bool = true
     private var common: Common = Common.init(false)
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    
+    // MARK: - Class Lifecycle Functions
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        // Set the mode button
+        self.modeButton.title = self.renderAsDark ? "Light" : "Dark"
+        
+        // Centre the main window and display
+        self.window.center()
+        self.window.makeKeyAndOrderFront(self)
+    }
+    
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+
+        // When the main window closed, shut down the app
+        return true
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
 
-
+    // MARK: - Action Functions
+    
     @IBAction private func doLoadYamlFile(_ sender: Any) {
 
         self.openDialog = NSOpenPanel.init()
-
         self.openDialog!.canChooseFiles = true
         self.openDialog!.canChooseDirectories = false
         self.openDialog!.allowsMultipleSelection = false
-
         self.openDialog!.delegate = self
         self.openDialog!.directoryURL = URL.init(fileURLWithPath: "")
 
         if self.openDialog!.runModal() == .OK {
-            var reportError: NSError? = nil
-
-            do {
-                if let yamlUrl: URL = self.openDialog!.url {
-                    self.filePathLabel.stringValue = yamlUrl.absoluteString
-
-                    // Get the file contents as a string
-                    let data: Data = try Data.init(contentsOf: yamlUrl, options: [.uncached])
-
-                    // Get the string's encoding, or fail back to .utf8
-                    let encoding: String.Encoding = data.stringEncoding ?? .utf8
-
-                    if let yamlFileString: String = String.init(data: data, encoding: encoding) {
-                        // Get the key string first
-                        let yamlAttString: NSAttributedString = common.getAttributedString(yamlFileString)
-
-                        self.previewTextView.backgroundColor = common.doShowLightBackground ? NSColor.init(white: 1.0, alpha: 0.9) : NSColor.textBackgroundColor
-                        self.previewScrollView.scrollerKnobStyle = common.doShowLightBackground ? .dark : .light
-
-                        if let renderTextStorage: NSTextStorage = self.previewTextView.textStorage {
-                            renderTextStorage.beginEditing()
-                            renderTextStorage.setAttributedString(yamlAttString)
-                            renderTextStorage.endEditing()
-
-                            // Add the subview to the instance's own view and draw
-                            //self.mainView.display()
-                            return
-                        }
-
-                        // We can't access the preview NSTextView's NSTextStorage
-                        reportError = setError(BUFFOON_CONSTANTS.ERRORS.CODES.BAD_TS_STRING)
-                    } else {
-                        // FROM 1.1.2
-                        // We couldn't convert to data to a valid encoding
-                        let errDesc: String = "\(BUFFOON_CONSTANTS.ERRORS.MESSAGES.BAD_TS_STRING) \(encoding)"
-                        reportError = NSError(domain: BUFFOON_CONSTANTS.APP_CODE_PREVIEWER,
-                                              code: BUFFOON_CONSTANTS.ERRORS.CODES.BAD_MD_STRING,
-                                              userInfo: [NSLocalizedDescriptionKey: errDesc])
-                    }
-                }
-            } catch {
-                // We couldn't read the file so set an appropriate error to report back
-                reportError = setError(BUFFOON_CONSTANTS.ERRORS.CODES.FILE_WONT_OPEN)
+            self.currentURL = self.openDialog!.url
+            let possibleError: NSError? = renderContent(self.openDialog!.url)
+            if possibleError != nil {
+                let errorAlert: NSAlert = NSAlert.init(error: possibleError!)
+                errorAlert.beginSheetModal(for: self.window)
             }
         }
 
         self.openDialog = nil
     }
+    
+    
+    @IBAction private func doSwitchMode(_ sender: Any) {
+        
+        self.renderAsDark = !self.renderAsDark
+        self.modeButton.title = self.renderAsDark ? "Light" : "Dark"
+        
+        let possibleError: NSError? = renderContent(self.currentURL)
+        if possibleError != nil {
+            // Pop up an alert
+            let errorAlert: NSAlert = NSAlert.init(error: possibleError!)
+            errorAlert.beginSheetModal(for: self.window)
+        }
+    }
 
+    
+    // MARK: - Rendering Functions
+    
+    func renderContent(_ fileToRender: URL?) -> NSError? {
+        
+        var reportError: NSError? = nil
 
+        do {
+            if let yamlUrl: URL = fileToRender {
+                self.window.title = yamlUrl.absoluteString
+
+                // Get the file contents as a string
+                let data: Data = try Data.init(contentsOf: yamlUrl, options: [.uncached])
+
+                // Get the string's encoding, or fail back to .utf8
+                let encoding: String.Encoding = data.stringEncoding ?? .utf8
+
+                if let yamlFileString: String = String.init(data: data, encoding: encoding) {
+                    common.doShowLightBackground = !self.renderAsDark
+                    common.resetStylesOnModeChange()
+                    let yamlAttString: NSAttributedString = common.getAttributedString(yamlFileString)
+
+                    self.previewTextView.backgroundColor = common.doShowLightBackground ? NSColor.init(white: 1.0, alpha: 0.9) : NSColor.textBackgroundColor
+                    self.previewScrollView.scrollerKnobStyle = common.doShowLightBackground ? .dark : .light
+
+                    if let renderTextStorage: NSTextStorage = self.previewTextView.textStorage {
+                        renderTextStorage.beginEditing()
+                        renderTextStorage.setAttributedString(yamlAttString)
+                        renderTextStorage.endEditing()
+                        return nil
+                    }
+
+                    // We can't access the preview NSTextView's NSTextStorage
+                    reportError = setError(BUFFOON_CONSTANTS.ERRORS.CODES.BAD_TS_STRING)
+                } else {
+                    // We couldn't convert to data to a valid encoding
+                    let errDesc: String = "\(BUFFOON_CONSTANTS.ERRORS.MESSAGES.BAD_TS_STRING) \(encoding)"
+                    reportError = NSError(domain: BUFFOON_CONSTANTS.APP_CODE_PREVIEWER,
+                                          code: BUFFOON_CONSTANTS.ERRORS.CODES.BAD_MD_STRING,
+                                          userInfo: [NSLocalizedDescriptionKey: errDesc])
+                }
+            } else {
+                // No file selected
+                let errDesc: String = "No file selected to render"
+                reportError = NSError(domain: BUFFOON_CONSTANTS.APP_CODE_PREVIEWER,
+                                      code: BUFFOON_CONSTANTS.ERRORS.CODES.BAD_MD_STRING,
+                                      userInfo: [NSLocalizedDescriptionKey: errDesc])
+            }
+        } catch {
+            // We couldn't read the file so set an appropriate error to report back
+            reportError = setError(BUFFOON_CONSTANTS.ERRORS.CODES.FILE_WONT_OPEN)
+        }
+        
+        return reportError
+    }
+    
+    
     /**
      Generate an NSError for an internal error, specified by its code.
 
@@ -126,5 +174,5 @@ class AppDelegate:  NSObject,
                        code: code,
                        userInfo: [NSLocalizedDescriptionKey: errDesc])
     }
-}
 
+}
