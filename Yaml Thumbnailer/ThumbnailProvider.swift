40e115b88214160a5764688ce508ff0fcfcd1d99
smittytone <smittytone@icloud.com>
2024-04-27 13:22:38 +0100

    Sync thumbnailer code across PreviewApps

--- Yaml Thumbnailer/ThumbnailProvider.swift
+++ Yaml Thumbnailer/ThumbnailProvider.swift
@@ -7,19 +7,20 @@
  */
 
 
+import Foundation
+import AppKit
 import QuickLookThumbnailing
-import Cocoa
 
 
 class ThumbnailProvider: QLThumbnailProvider {
 
     // MARK:- Private Properties
-    
-    // FROM 1.1.0
-    // Add Errors the may be returned by autoreleasepool closure
+
     private enum ThumbnailerError: Error {
         case badFileLoad(String)
         case badFileUnreadable(String)
+        case badFileUnsupportedEncoding(String)
+        case badFileUnsupportedFile(String)
         case badGfxBitmap
         case badGfxDraw
     }
@@ -32,7 +33,7 @@ class ThumbnailProvider: QLThumbnailProvider {
         /*
          * This is the main entry point for the macOS thumbnailing system
          */
-        
+
         // Load the source file using a co-ordinator as we don't know what thread this function
         // will be executed in when it's called by macOS' QuickLook code
         if FileManager.default.isReadableFile(atPath: request.fileURL.path) {
@@ -42,15 +43,17 @@ class ThumbnailProvider: QLThumbnailProvider {
                 // as we're not going to read it again any time soon
                 let data: Data = try Data.init(contentsOf: request.fileURL, options: [.uncached])
 
-                // FROM 1.1.2
                 // Get the string's encoding, or fail back to .utf8
                 let encoding: String.Encoding = data.stringEncoding ?? .utf8
 
+                // Check the string's encoding generates a valid string
+                // NOTE This may not be necessary and so may be removed
                 guard let yamlFileString: String = String.init(data: data, encoding: encoding) else {
-                    return handler(nil, ThumbnailerError.badFileLoad(request.fileURL.path))
+                    handler(nil, ThumbnailerError.badFileLoad(request.fileURL.path))
+                    return
                 }
 
-                // Instatiate the common code
+                // Instantiate the common code
                 let common: Common = Common.init(true)
 
                 // Set the primary drawing frame and a base font size
@@ -59,36 +62,32 @@ class ThumbnailProvider: QLThumbnailProvider {
                                                    CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.WIDTH),
                                                    CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.HEIGHT))
 
-                // FROM 1.0.1
-                // Instantiate an NSTextField to display the NSAttributedString render of the YAML,
-                // and extend the size of its frame
+                // Instantiate an NSTextField to display the NSAttributedString render of the YAML
                 let yamlTextField: NSTextField = NSTextField.init(frame: yamlFrame)
                 yamlTextField.attributedStringValue = common.getAttributedString(yamlFileString)
 
                 // Generate the bitmap from the rendered YAML text view
                 guard let bodyImageRep: NSBitmapImageRep = yamlTextField.bitmapImageRepForCachingDisplay(in: yamlFrame) else {
-                    return handler(nil, ThumbnailerError.badGfxBitmap)
+                    handler(nil, ThumbnailerError.badGfxBitmap)
+                    return
                 }
 
-                // Draw into the bitmap first the YAML view...
+                // Draw the YAML view into the bitmap
                 yamlTextField.cacheDisplay(in: yamlFrame, to: bodyImageRep)
 
                 if let image: CGImage = bodyImageRep.cgImage {
+                    // Just in case, make a copy of the cgImage, in case
+                    // `bodyImageReg` is freed
                     if let cgImage: CGImage = image.copy() {
-                        // Set the thumbnail frame
-                        // NOTE This is always square, with height matched to width, so adjust
-                        //      to a 3:4 aspect ratio to maintain the macOS standard doc icon width
-                        let iconScale: CGFloat = request.scale
+                        // Calculate image scaling, frame size, etc.
                         let thumbnailFrame: CGRect = NSMakeRect(0.0,
                                                                 0.0,
                                                                 CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ASPECT) * request.maximumSize.height,
                                                                 request.maximumSize.height)
-
-
                         let scaleFrame: CGRect = NSMakeRect(0.0,
                                                             0.0,
-                                                            thumbnailFrame.width * iconScale,
-                                                            thumbnailFrame.height * iconScale)
+                                                            thumbnailFrame.width * request.scale,
+                                                            thumbnailFrame.height * request.scale)
 
                         // Pass a QLThumbnailReply and no error to the supplied handler
                         handler(QLThumbnailReply.init(contextSize: thumbnailFrame.size) { (context) -> Bool in
