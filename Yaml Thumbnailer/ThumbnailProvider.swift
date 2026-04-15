fbeb4c4519af59034b30ae4342b10e3683d48f55
Tony Smith (smittytone) <tony@smittytone.net>
2026-04-15 10:50:26 +0100

    Handle Tahoe thumbnail colouring

--- Yaml Thumbnailer/ThumbnailProvider.swift
+++ Yaml Thumbnailer/ThumbnailProvider.swift
@@ -42,15 +42,15 @@ class ThumbnailProvider: QLThumbnailProvider {
                 // Get the file contents as a string, making sure it's not cached
                 // as we're not going to read it again any time soon
                 //let data: Data = try Data.init(contentsOf: request.fileURL, options: [.uncached])
-                let jsonFileHandle = try FileHandle(forReadingFrom: request.fileURL)
-                try jsonFileHandle.seek(toOffset: 0)
-                guard let data = try jsonFileHandle.read(upToCount: 1024) else {
-                    try jsonFileHandle.close()
+                let yamlFileHandle = try FileHandle(forReadingFrom: request.fileURL)
+                try yamlFileHandle.seek(toOffset: 0)
+                guard let data = try yamlFileHandle.read(upToCount: 1024) else {
+                    try yamlFileHandle.close()
                     handler(nil, ThumbnailerError.badFileUnreadable(request.fileURL.path))
                     return
                 }
 
-                try jsonFileHandle.close()
+                try yamlFileHandle.close()
 
                 // Get the string's encoding, or fail back to .utf8
                 let encoding: String.Encoding = data.stringEncoding ?? .utf8
@@ -75,6 +75,18 @@ class ThumbnailProvider: QLThumbnailProvider {
                 let yamlTextField: NSTextField = NSTextField.init(frame: yamlFrame)
                 yamlTextField.attributedStringValue = common.getThumbnailString(fromYaml: yamlFileString)
 
+                // FROM 2.0.0
+                // From macOS 26.1, make sure thumbnail backgrounds remain white
+                // NOTE This may become a setting in future, but for now retain the styling
+                //      we have always presented.
+                if #available(macOS 26.1, *) {
+                    if !common.settings.thumbnailMatchFinderMode {
+                        yamlTextField.isBezeled = false
+                        yamlTextField.drawsBackground = true
+                        yamlTextField.backgroundColor = .white
+                    }
+                }
+
                 // Generate the bitmap from the rendered YAML text view
                 guard let bodyImageRep: NSBitmapImageRep = yamlTextField.bitmapImageRepForCachingDisplay(in: yamlFrame) else {
                     handler(nil, ThumbnailerError.badGfxBitmap)
@@ -85,27 +97,24 @@ class ThumbnailProvider: QLThumbnailProvider {
                 yamlTextField.cacheDisplay(in: yamlFrame, to: bodyImageRep)
 
                 if let image: CGImage = bodyImageRep.cgImage {
-                    // Just in case, make a copy of the cgImage, in case
-                    // `bodyImageReg` is freed
-                    if let cgImage: CGImage = image.copy() {
-                        // Calculate image scaling, frame size, etc.
-                        let thumbnailFrame: CGRect = NSMakeRect(0.0,
-                                                                0.0,
-                                                                CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ASPECT) * request.maximumSize.height,
-                                                                request.maximumSize.height)
-                        let scaleFrame: CGRect = NSMakeRect(0.0,
+                    // Calculate image scaling, frame size, etc.
+                    let thumbnailFrame: CGRect = NSMakeRect(0.0,
                                                             0.0,
-                                                            thumbnailFrame.width * request.scale,
-                                                            thumbnailFrame.height * request.scale)
+                                                            CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ASPECT) * request.maximumSize.height,
+                                                            request.maximumSize.height)
+                    let scaleFrame: CGRect = NSMakeRect(10.0,
+                                                        20.0,
+                                                        thumbnailFrame.width * request.scale - 10.0,
+                                                        thumbnailFrame.height * request.scale - 20.0)
 
-                        // Pass a QLThumbnailReply and no error to the supplied handler
-                        handler(QLThumbnailReply.init(contextSize: thumbnailFrame.size) { (context) -> Bool in
-                            // `scaleFrame` and `cgImage` are immutable
-                            context.draw(cgImage, in: scaleFrame, byTiling: false)
-                            return true
-                        }, nil)
-                        return
-                    }
+                    // Pass a QLThumbnailReply and no error to the supplied handler
+                    handler(QLThumbnailReply.init(contextSize: thumbnailFrame.size) { (context) -> Bool in
+                        // `scaleFrame` and `cgImage` are immutable
+                        context.draw(image, in: scaleFrame, byTiling: false)
+                        return true
+                    }, nil)
+                    
+                    return
                 }
 
                 handler(nil, ThumbnailerError.badGfxDraw)
